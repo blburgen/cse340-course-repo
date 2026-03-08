35516b7d9ce142b37511b31887c008c6ce8f45e2
Brady Burgener <bradylbsee@gmail.com>
2026-03-07 19:51:46 -0700

    Week 2 Database Retrieval

--- src/models/setup.sql
+++ src/models/setup.sql
@@ -11,3 +11,42 @@ VALUES
 ('BrightFuture Builders', 'A nonprofit focused on improving community infrastructure through sustainable construction projects.', 'info@brightfuturebuilders.org', 'brightfuture-logo.png'),
 ('GreenHarvest Growers', 'An urban farming collective promoting food sustainability and education in local neighborhoods.', 'contact@greenharvest.org', 'greenharvest-logo.png'),
 ('UnityServe Volunteers', 'A volunteer coordination group supporting local charities and service initiatives.', 'hello@unityserve.org', 'unityserve-logo.png');
+
+CREATE TABLE category (
+	category_id SERIAL PRIMARY KEY,
+	name VARCHAR(75) NOT NULL
+);
+
+CREATE TABLE service_project (
+	service_project_id SERIAL PRIMARY KEY,
+	name VARCHAR(150) NOT NULL,
+	description TEXT NOT NULL
+);
+
+CREATE TABLE category_service (
+	category_id INTEGER,
+	service_project_id INTEGER,
+	PRIMARY KEY (service_project_id,category_id),
+	FOREIGN KEY (service_project_id) REFERENCES service_project (service_project_id),
+	FOREIGN KEY (category_id) REFERENCES category (category_id)
+);
+
+INSERT INTO category (name)
+VALUES
+('Enironmental'),
+('Educational'),
+('Community Service'),
+('Health and Wellness');
+
+INSERT INTO service_project (name,description)
+VALUES
+('Park Cleanup','Join us to clean up local parks and make them beautiful!'),
+('Food Drive','Help collect and distribute food to those in need.'),
+('Community Tutoring','Volunteer to tutor students in various subjects.');
+
+INSERT INTO category_service (category_id,service_project_id)
+VALUES
+(1,1),
+(2,3),
+(3,1),(3,2),(3,3),
+(4,1),(4,2);
