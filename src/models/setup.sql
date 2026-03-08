b7c6820c0aa773b00482d235d4dfdf2b7a01770e
Brady Burgener <bradylbsee@gmail.com>
2026-03-08 10:37:35 -0600

    updated service_projects to include 15

--- src/models/setup.sql
+++ src/models/setup.sql
@@ -34,6 +34,24 @@ CREATE TABLE service_project (
 	FOREIGN KEY (organization_id) REFERENCES organization (organization_id)
 );
 
+INSERT INTO service_project (organization_id,title,description,location,date)
+VALUES
+(3,'Park Cleanup','Join us to clean up local parks and make them beautiful!','','06/12/2026'), -- 1
+(3,'Food Drive','Help collect and distribute food to those in need.','Utah Food Bank','05/10/2026'), -- 2
+(3,'Community Tutoring','Volunteer to tutor students in various subjects.','local schools','Tuesdays'), -- 3
+(3,'Make Cards For Seniors','Come join us to uplift our seniors, by making thank you cards','Next left church','3/20/2026'), -- 4
+(3,'Become A Mentor','Come volunteer to become a mentor for those looking to get a job','Work services building','Wednesdays'), -- 5
+(1,'Build Swing Set','Volunteer to help set up a swing set in Rock River Park','Rock River Park','4/1/2026'), -- 6
+(1,'Build Local Community Garden','Help us build a community garden next to Rim Rock Park','Rim Rock Park','5/1/2026'), -- 7
+(1,'Trash Clean Up','Our roads need some TLC.  Come help the community clean up trash on our roads.','Get Good Park','4/15/2026'), -- 8
+(1,'Public Mural','Our local grocery store would like to beautify its building.  Come help paint a public mural.','Tom Grocery Store','3/20/2026'), -- 9
+(1,'Clean Graffiti','Help clean up our train station by removing graffiti.','Roger Train Station','4/25/2026'), -- 10
+(2,'Community Garden Opening','Join us in planting our community garden and git a pot to take home with you.','Green River Community Garden','3/20/2026'), -- 11
+(2,'Plant A Tree','Help to beautify our local community by planting a free tree in your yard.','Green River Green House','4/11/2026'), -- 12
+(2,'Plant Vegitables','Come plan some vegitable in our community garden and take home some for yourself.','Rock Road Community Garden','4/1/2026'), -- 13
+(2,'Green House Construction','Our community garden needs a new green house.  Please come and help put it up.','Rocky Sanders Gardens','5/1/2026'), -- 14
+(2,'Learn To Grow','Help others learn how to keep a garden or come learn how to keep a garden.','Sam Community Park','3/25/2026'); -- 15
+
 CREATE TABLE category_service (
 	category_id INTEGER,
 	service_project_id INTEGER,
@@ -42,15 +60,9 @@ CREATE TABLE category_service (
 	FOREIGN KEY (category_id) REFERENCES category (category_id)
 );
 
-INSERT INTO service_project (title,description)
-VALUES
-('Park Cleanup','Join us to clean up local parks and make them beautiful!'),
-('Food Drive','Help collect and distribute food to those in need.'),
-('Community Tutoring','Volunteer to tutor students in various subjects.');
-
 INSERT INTO category_service (category_id,service_project_id)
 VALUES
-(1,1),
-(2,3),
-(3,1),(3,2),(3,3),
-(4,1),(4,2);
+(1,1),(1,7),(1,8),(1,10),(1,11), -- Enironmental
+(2,3),(2,5),(2,11),(2,12),(2,15), -- Educational
+(3,1),(3,2),(3,3),(3,4),(3,6),(3,7),(3,8),(3,9),(3,10),(3,14), -- Community Service
+(4,1),(4,2),(4,4),(4,7),(4,8),(4,11),(4,12),(4,15); -- Health and Wellness
