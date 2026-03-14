fefa6c9f7e2eae9019fa8dac7b9ff4d8cd66122b
Brady Burgener <bradylbsee@gmail.com>
2026-03-13 18:01:50 -0600

    W03 parameters

--- src/models/projects.js
+++ src/models/projects.js
@@ -12,6 +12,26 @@ const getAllProjects = async() => {
   const result = await db.query(query);
 
   return result.rows;
-}
+};
 
-export {getAllProjects}  
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          service_project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          date
+        FROM service_project
+        WHERE organization_id = $1
+        ORDER BY date;
+      `;
+      
+      const query_params = [organizationId];
+      const result = await db.query(query, query_params);
+
+      return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId };
